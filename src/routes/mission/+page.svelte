05ffe0410082168ef054083945d9e20c4b1001ea
cobular <22972550+Cobular@users.noreply.github.com>
2022-11-12 15:31:24 -0800

    Added who we are

--- src/routes/mission/+page.svelte
+++ src/routes/mission/+page.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
 	import Content from "$lib/components/general/content.svelte";
+	import WhoWeAre from "$lib/sections/who-we-are.svelte";
 
 </script>
 
 <Content>
   <h1 class="text-6xl">mission & history</h1>
 </Content>
+
+<WhoWeAre />
 
 
 <svelte:head>
