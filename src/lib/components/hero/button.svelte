51233c334c9d474fe519ac3160fc0c60ad05818f
cobular <22972550+Cobular@users.noreply.github.com>
2022-11-17 00:26:08 -0800

    Remove metrics from dev site, added url constraints to metrics

--- src/lib/components/hero/button.svelte
+++ src/lib/components/hero/button.svelte
@@ -1,8 +1,8 @@
 <a
-	class="rounded-md bg-[#FDDE97] px-14 py-3 hover:bg-[#fee3a5] active:bg-[#ffeab8] umami--click--button-main"
+	class="rounded-md bg-[#FDDE97] px-12 sm:px-14 py-3 hover:bg-[#fee3a5] active:bg-[#ffeab8] umami--click--button-main"
 	href="https://tinyurl.com/QWERHacks23"
 >
 	<p
-		class="text-2xl md:text-3xl"
+		class="text-2xl md:text-3xl whitespace-nowrap"
 	>Apply Now!</p>
 </a>
