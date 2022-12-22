8d61b8d713cb51629207572b84290c70f0df7115
aliiyuu <yu.alicia@gmail.com>
2022-12-21 16:01:05 -0800

    WIP: 5023732 test

---
+++ src/lib/components/prizes/making_memories.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+	import ImageWrapper from './card_templates/image_wrapper.svelte';
+	import RowCard from './card_templates/row_card.svelte';
+</script>
+
+<RowCard title="Making Memories">
+	<svelte:fragment slot="row-1">
+		<ImageWrapper
+			src={'media/component_assets/prizes/sleep/Rectangle 5.png'}
+			alt="Weighted blanket"
+      bg_color="rgba(255, 144, 119, 0.1)"
+		/>
+		<ImageWrapper
+			src={'media/component_assets/prizes/sleep/Rectangle 2.png'}
+			alt="Deep sleep pillow spray"
+      bg_color="rgb(45, 64, 97)"
+		/>
+		<ImageWrapper
+			src={'media/component_assets/prizes/sleep/Rectangle 1.png'}
+			alt="Weighted blanket"
+      bg_color="rgba(167, 221, 254, 0.42)"
+		/>
+	</svelte:fragment>
+	<svelte:fragment slot="row-2">
+		<ImageWrapper
+			src={'media/component_assets/prizes/sleep/Rectangle 6.png'}
+			alt="Weighted blanket"
+      bg_color="rgba(235, 204, 142, 0.24)"
+		/>
+		<ImageWrapper
+			src={'media/component_assets/prizes/sleep/Rectangle 3.png'}
+			alt="Weighted blanket"
+      bg_color="rgba(255, 144, 119, 0.13)"
+		/>
+		<div class="w-[35ch] max-w-[40%] min-w-[30%]">
+			<ul class="text-sm flex flex-col gap-2 list-outside pl-3 leading-[1.1]">
+				<li>Fuji Instax Camera</li>
+				<li>Instax Film 20 Sheet</li>
+				<li>Scuddles 60 by 79 Inch Picnic Blanket</li>
+				<li>Unstable Unicorns</li>
+			</ul>
+		</div>
+	</svelte:fragment>
+</RowCard>
