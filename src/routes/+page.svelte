dc61b96f1d95d87837074187e407d70dbdd0224a
Mia Taylor <miastaylor1@gmail.com>
2023-12-10 16:16:35 -0800

    added proper background masks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,15 @@
 
 <Hero />
 <div class="w-full dots one" style="">
-	<Info />
+    <div class="w-full h-full mask-overlay one" />
+    <Info />
 </div>
 <div class="w-full dots two" style="">
+    <div class="w-full h-full mask-overlay two" />
 	<FAQ />
 </div>
 <div class="w-full dots three">
+    <div class="w-full h-full mask-overlay three" />
 	<Donate />
 </div>
 
@@ -36,5 +39,27 @@
             background-image: $mask, url("https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi0.wp.com%2Fwww.heroicgirls.com%2Fwp-content%2Fuploads%2F2021%2F02%2Fcomic-book-dots-transparent-png-clipart-free-download-ywd-benday-dots-png-3600_3600.png%3Fssl%3D1&f=1&nofb=1&ipt=8cf13501f1403f762fa2ffcc75dbeebdf3c124018f73412e9e0ae7aa0a5a0b3c&ipo=images"), linear-gradient(180deg, #F4C0CA 0%, #C993FF 100%);
             background-position-x: -50vw, 0px, center;
         }
+    }
+    .mask-overlay {
+        position: absolute;
+        &.one {
+            margin-top: -15vh;
+            background: linear-gradient(180deg, #A84FB3 0%, #E689C6 99.48%);
+            mask-image: radial-gradient(black 30%, transparent 90%);
+            height: 100vh;
+        }
+        &.two {
+            background: linear-gradient(180deg, #E688C6 0%, #F4C1CA 100%);
+            mask-image: radial-gradient(black 30%, transparent 90%);
+            height: 100vh;
+            right: 50vw;
+        }
+        &.three {
+            background: linear-gradient(180deg, #F4C0CA 0%, #C993FF 100%);
+            mask-image: radial-gradient(black 30%, transparent 90%);
+            height: 100vh;
+            left: 25vw;
+        }
+        isolation: isolate;
     }
 </style>
