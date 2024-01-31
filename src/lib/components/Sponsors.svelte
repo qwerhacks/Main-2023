fb86bc6aab12f0aa360a6075bc3e1689f11ceea9
Julie <22972550+Cobular@users.noreply.github.com>
2024-01-30 21:58:10 -0800

    Logos added!

--- src/lib/components/Sponsors.svelte
+++ src/lib/components/Sponsors.svelte
@@ -35,18 +35,15 @@
 		</WindowBase>
 	</div>
 	<div class="right justify-items-center gap-4">
-		<div class="px-8">
+		<div class="">
 			<WindowBase
-				title="Sponsors"
+				title="Gold Sponsors"
 				textCenter={true}
 				background_val="linear-gradient(90deg, #FDDE97 0.09%, #B79A57 100%)"
 				shadow={false}
 			>
-				<a class="content" href="https://mule.to/p5i8">
-					<img src="assets/sponsors/sticker-mule-logo-light-bg.svg" alt="StickerMule" width="400" />
-				</a>
-				<a class="content" href="https://aerospace.org/">
-					<img src="assets/sponsors/aerospace.svg" alt="Aerospace Corp" width="400" />
+				<a class="content" href="https://www.ppg.com/en-US">
+					<img src="assets/sponsors/ppg.svg" alt="PPG" width="200" />
 				</a>
 			</WindowBase>
 		</div>
@@ -58,25 +55,11 @@
 				shadow={false}
 			>
 				<div class="content">
-					<img
-						src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg"
-						alt="Google"
-						width="60"
-						height="60"
-					/>
-					<img
-						src="https://upload.wikimedia.org/wikipedia/commons/9/99/Lockheed_Martin_logo.svg"
-						alt="Lockheed"
-						width="100"
-						height="40"
-					/>
-					<img
-						src="https://upload.wikimedia.org/wikipedia/commons/3/36/Northrop_Grumman_logo_blue-on-clear_2020.svg"
-						alt="Northrup"
-						width="200"
-					/>
-				</div>
-			</WindowBase>
+					<a class="content" href="https://www.northropgrumman.com/">
+						<img src="assets/sponsors/northrop.svg" alt="Northrop Grumman" width="200" />
+					</a>
+				</div></WindowBase
+			>
 		</div>
 		<div class="pb-10">
 			<WindowBase
@@ -84,7 +67,40 @@
 				textCenter={true}
 				background_val="linear-gradient(90deg, #98564D 0.09%, #B66E57 100%)"
 				shadow={false}
-			></WindowBase>
+			>
+				<div class="flex flex-row flex-wrap justify-around gap-2">
+					<a class="content" href="https://www.crowdstrike.com/en-us/">
+						<img src="assets/sponsors/crowdstrike.png" alt="CrowdStrike" width="200" />
+					</a>
+					<a class="content" href="https://www.directv.com/">
+						<img src="assets/sponsors/directv.svg" alt="DirecTV" width="200" />
+					</a>
+					<a class="content px-6" href="https://www.chevron.com/">
+						<img src="assets/sponsors/chevron.svg" alt="Chevron" width="200" />
+					</a>
+					<a class="content" href="https://holoash.com/">
+						<img src="assets/sponsors/holoash_inverted.png" alt="HoloAsh" width="200" />
+					</a>
+					<a class="content" href="https://aerospace.org/">
+						<img src="assets/sponsors/aerospace.svg" alt="The Aerospace Corporation" width="200" />
+					</a>
+					<a class="content" href="https://www.boeing.com/">
+						<img src="assets/sponsors/boeing.svg" alt="Boeing" width="200" />
+					</a>
+					<a class="content" href="https://www.sce.com/">
+						<img src="assets/sponsors/edison.svg" alt="Southern California Edison" width="200" />
+					</a>
+					<a class="content" href="https://www.illumina.com/">
+						<img src="assets/sponsors/illumina.svg" alt="Illumina" width="200" />
+					</a>
+					<a class="content" href="https://cloud.google.com/">
+						<img src="assets/sponsors/gcp.svg" alt="Google Cloud" width="200" />
+					</a>
+					<a class="content" href="https://www.lockheedmartin.com/">
+						<img src="assets/sponsors/lockheed.svg" alt="Lockheed Martin" width="200" />
+					</a>
+				</div>
+			</WindowBase>
 		</div>
 	</div>
 </div>
@@ -106,7 +122,7 @@
 		flex-wrap: wrap;
 		flex-direction: row;
 		justify-content: flex-start;
-		align-items: flex-start;
+		align-content: center;
 		gap: 25px;
 	}
 	.sponsors-text {
